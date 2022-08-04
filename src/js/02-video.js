dd831a032f3d5528df2b7032149fe12e8c951a1d
OttoCarius <ottocarius1993@gmail.com>
2022-08-04 16:04:34 +0300

    task 2 - done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,2 +1,29 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
+
+const player = new Player('vimeo-player', {
+  id: '236203659',
+  width: 1024,
+});
+
+const storegeCurrentTime = function (data) {
+  localStorage.setItem('video-current-time', data.seconds);
+};
+player.on('timeupdate', throttle(storegeCurrentTime, 1000));
+
+player
+  .setCurrentTime(localStorage.getItem('video-current-time'))
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
