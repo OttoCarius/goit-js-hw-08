883dd9c7030dc2d08de8c25b58f86031fec93ac6
OttoCarius <ottocarius1993@gmail.com>
2022-08-04 15:30:25 +0300

    add start packet + 1 task - done

---
+++ src/js/01-gallery.js
@@ -0,0 +1,20 @@
+import { galleryItems } from './gallery-items';
+
+import SimpleLightbox from 'simplelightbox';
+
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const markup = galleryItems.map(el => {
+  return `<a class="gallery__item" href="${el.original}">
+      <img class="gallery__image" src="${el.preview}" alt="${el.description}" />
+    </a>`;
+});
+
+const gallery = document.querySelector('.gallery');
+
+gallery.insertAdjacentHTML('afterbegin', markup.join(''));
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: '250',
+});
